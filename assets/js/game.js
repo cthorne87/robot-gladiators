a92434d74ee0713346e48387be8256cd6ba3f184
Chris Thorne <u1400900@umail.utah.edu>
2021-11-03 20:48:38 -0600

    commit2

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("this is an alert! JavaScript is running!");
+//var playerName = window.prompt("What is your robot's name?");
+//What is this?
+//console.log(playerName);
+
+//function fight() {
+//    window.alert("The fight has begun!");   
+//}
+//fight()
+var name = "Chris";
+console.log(name);
