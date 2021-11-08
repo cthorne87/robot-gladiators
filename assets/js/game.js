c14cacfa91553f2900ca8c7fe45630bbfaca8616
Chris Thorne <u1400900@umail.utah.edu>
2021-11-07 20:57:22 -0700

    start and endgame functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -51,7 +51,7 @@ var fight = function(enemyName) {
     );
 
     if (playerHealth <= 0) {
-      window.alert(playerName + ' has died!');
+      endGame();
       break;
     } else {
       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
@@ -59,19 +59,48 @@ var fight = function(enemyName) {
   }
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
-  if (playerHealth > 0) {
-    window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+var startGame = function() {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10
 
-    var pickedEnemyName = enemyNames[i];
+    for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
-    enemyHealth = 50;
+        var pickedEnemyName = enemyNames[i];
+
+        enemyHealth = 50;
 
 
-    fight(pickedEnemyName);
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert('You have lost your robot in battle! Game Over!');
+        break;
+      }
+    }
+
+};
+
+
+
+var endGame = function(){
+  if (playerHealth > 0){
+    window.alert("Great job, you've survived the game! You now have a score")
   }
   else {
-    window.alert('You have lost your robot in battle! Game Over!');
-    break;
+    window.alert("You've lost your robot in battle.")
   }
 }
+
+var playAgainConfirm = window.confirm("Would you like to play again?") 
+
+if (playAgainConfirm) {
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+  }
+
+  startGame();
